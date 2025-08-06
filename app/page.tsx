2e2ac8533cb1e89a9599c98ae5f6f6081c5bbecb
oelsheha <oelsheha@sfu.ca>
2025-08-05 19:11:35 -0700

    chore: reorganizing file strucutre, some shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { createClient } from '../server/supabase/server'
 import { cookies } from 'next/headers'
 import { Todo } from '../lib/types/index'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+
 
 export default async function Page() {
   const cookieStore = await cookies()
@@ -10,12 +13,23 @@ export default async function Page() {
 
 
   return (
-    <div>
-    <ul>
-      {todos?.map((todo, index) => (
-          <li key={index}>{todo.todo}</li>
-        ))}
-      </ul>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <Card className='w-full max-w-md'>
+        <CardHeader>
+          <CardTitle>Todos</CardTitle>
+          <CardDescription>A list of your todos</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='flex flex-col gap-2'>
+            {todos?.map((todo, index) => (
+              <div key={index} className='flex items-center justify-between'>
+                <p>{todo.todo}</p>
+                <Button>Delete</Button>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
